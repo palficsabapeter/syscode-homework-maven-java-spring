a8ae60bbb4bd0b082a667fcf4e8cec82bb118724
palficsabapeter <palficsabapeter@gmail.com>
2024-01-27 10:07:06 +0100

    Initial commit

---
+++ src/main/java/hu/syscode/Main.java
@@ -0,0 +1,17 @@
+package hu.syscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SpringBootApplication
+public class Main {
+	private static final Logger logger = LogManager.getLogger(Main.class);
+	
+    public static void main(String[] args) {
+    	logger.info("Starting application");
+
+        SpringApplication.run(Main.class, args);
+    }
+}
